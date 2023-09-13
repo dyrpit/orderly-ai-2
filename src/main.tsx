57558ac9a9c57c95d5949ef29a433f726d9bff59
Justyna <lempickajustyna@gmail.com>
2023-09-13 12:40:18 +0200

    initialize router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
+import { AdminPage, ProductPage } from './Pages/index.ts';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/admin",
+        element: <AdminPage />,
+        handle: {
+          crumb: () => "admin panel"
+        },
+      },
+      {
+        path: "/product",
+        element: <ProductPage />,
+        handle: {
+          crumb: () => "product page"
+        },
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
