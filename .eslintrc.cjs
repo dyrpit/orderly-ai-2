11969b7082801eeefcd5a62495e1a22e354f872c
Justyna <lempickajustyna@gmail.com>
2023-09-13 11:04:15 +0200

    prettier linter config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,13 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        "max": 2,
+        "maxEOF": 1,
+        "maxBOF": 0
+      }
+    ]
   },
-}
+};
