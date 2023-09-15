80d84034051e502d7bd4aea7acd2c1bb3440f813
Hituh <okazysssie@gmail.com>
2023-09-15 09:32:02 +0200

    Assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { AdminNavbar, LoginModal } from "./Components";
 
 function App() {
-  return <></>;
-
+  return <><LoginModal/></>;
+}
 export default App;
