a776e4922e585755b17bb29fb16443ac3ac8453a
justynalem <102022599+justynalem@users.noreply.github.com>
2023-09-18 14:33:00 +0200

    finally navbar (#46)

--- src/Layout/RootLayout.tsx
+++ src/Layout/RootLayout.tsx
@@ -1,17 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import { Footer, MainContainer } from '../Components'
-import Breadcrumbs from '../Components/Breadcrumps'
+import { Outlet } from 'react-router-dom';
+import { Footer, MainContainer } from '../Components';
+import Breadcrumbs from '../Components/Breadcrumps';
+import { NavBar } from '../Components/NavBar/NavBar';
 
 export default function RootLayout() {
-    return(
+    return (
         <div className="RootLayout">
+            <NavBar />
             <MainContainer>
                 <main>
-                <Breadcrumbs />
+                    <Breadcrumbs />
                     <Outlet></Outlet>
                 </main>
             </MainContainer>
             <Footer></Footer>
         </div>
-    )
+    );
 }
