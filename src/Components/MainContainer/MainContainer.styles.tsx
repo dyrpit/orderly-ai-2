74f848f08629751499803ff194d6761cb5a04535
Justyna <lempickajustyna@gmail.com>
2023-09-18 21:38:17 +0200

    add admin content fixes

--- src/Components/MainContainer/MainContainer.styles.tsx
+++ src/Components/MainContainer/MainContainer.styles.tsx
@@ -3,20 +3,22 @@ import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 export const Container = styled(Box)(({ theme }) => ({
- backgroundColor: colorVar.mainBackground,
- display: "flex",
- width: "1170px",
- marginLeft: "auto",
- marginRight: "auto",
- [theme.breakpoints.down("laptop")]: {
-  width: "100%",
- },
+  backgroundColor: colorVar.mainBackground,
+  display: "flex",
+  width: "1170px",
+  marginLeft: "auto",
+  marginRight: "auto",
+  [theme.breakpoints.down("laptop")]: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center"
+  },
 }));
 
 export const ContainerContent = styled(Box)(() => ({
- display: "flex",
- flexWrap: "wrap",
- justifyContent: "center",
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
 }));
 
 const smallScreenStyles = `
