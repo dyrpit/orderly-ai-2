853930ffbe3d3b1310f13f7ca8f39b6cd86f67f5
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-09-18 15:06:29 +0200

    Global CSS comment, fix na kontenerze glownym oraz fix na footerze i jego wycentrowanie

--- src/Components/Footer/Footer.styles.tsx
+++ src/Components/Footer/Footer.styles.tsx
@@ -1,11 +1,19 @@
 import { Box, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
+export const FooterWrapper = styled(Box)(() => ({
+ display: "flex",
+ justifyContent: "center",
+}));
+
 export const FooterContainer = styled(Box)(({ theme: theme }) => ({
  backgroundColor: theme.palette.background.paper,
  width: "1170px",
  height: "70px",
  [theme.breakpoints.down("tablet")]: {
+  width: "100%",
+ },
+ [theme.breakpoints.down("laptop")]: {
   width: "100%",
  },
 }));
@@ -18,8 +26,5 @@ export const FooterContent = styled(Typography)(({ theme: theme }) => ({
  [theme.breakpoints.down("tablet")]: {
   width: "100%",
   fontSize: "14px",
- },
- [theme.breakpoints.down("laptop")]: {
-  width: "100%",
  },
 }));
--- src/Components/MainContainer/MainContainer.styles.tsx
+++ src/Components/MainContainer/MainContainer.styles.tsx
@@ -2,20 +2,22 @@ import { colorVar } from "../../Theme/variables";
 import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const Container = styled(Box)(() => ({
-    backgroundColor: colorVar.mainBackground,
-    display: 'flex',
-    width: "1170px",
-    height: '100vh',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-}))
+export const Container = styled(Box)(({ theme }) => ({
+ backgroundColor: colorVar.mainBackground,
+ display: "flex",
+ width: "1170px",
+ marginLeft: "auto",
+ marginRight: "auto",
+ [theme.breakpoints.down("laptop")]: {
+  width: "100%",
+ },
+}));
 
 export const ContainerContent = styled(Box)(() => ({
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-}))
+ display: "flex",
+ flexWrap: "wrap",
+ justifyContent: "center",
+}));
 
 const smallScreenStyles = `
   @media (max-width: 1170px) {
@@ -25,4 +27,4 @@ const smallScreenStyles = `
   }
 `;
 
-export default smallScreenStyles; 
+export default smallScreenStyles;
