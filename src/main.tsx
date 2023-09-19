ead19165a5f952a7b6fb5fcfeb0f72efb6d94fe4
Hituh <okazysssie@gmail.com>
2023-09-19 14:37:52 +0200

    Fix: Wrap app in ContextProviders

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,23 @@ import App from "./App.tsx";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./Theme/ThemeProvider.ts";
 import { UserContextProvider } from "./Context/userContext/UserContextProvider.tsx";
+import { CategoryContextProvider } from "./Context/categoryContext/CategoryContextProvider.tsx";
+import { ItemContextProvider } from "./Context/itemContext/ItemContextProvider.tsx";
+import { VariableContextProvider } from "./Context/variableContext/VariableContextProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <UserContextProvider>
+ <React.StrictMode>
+  <UserContextProvider>
+   <CategoryContextProvider>
+    <ItemContextProvider>
+     <VariableContextProvider>
+      {" "}
       <ThemeProvider theme={theme}>
-        <App />
+       <App />
       </ThemeProvider>
-    </UserContextProvider>
-  </React.StrictMode>,
+     </VariableContextProvider>
+    </ItemContextProvider>
+   </CategoryContextProvider>
+  </UserContextProvider>
+ </React.StrictMode>,
 );
