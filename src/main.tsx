a5593774627dbec2075d206dde26008abae7a620
Justyna <lempickajustyna@gmail.com>
2023-09-19 10:12:05 +0200

    add and wrap app with contextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./Theme/ThemeProvider.ts";
+import { OrderAiContextProvider } from "./Context/ContextProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeProvider>
+  <React.StrictMode>
+    <OrderAiContextProvider>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </OrderAiContextProvider>
+  </React.StrictMode>,
 );
