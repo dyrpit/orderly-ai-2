2af7bbfc725fdc0056599fb552476f0a7022c17d
Justyna <lempickajustyna@gmail.com>
2023-09-19 20:44:12 +0200

    add productList to product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home";
-import Products from "./Pages/Products";
 import RootLayout from "./Layout/RootLayout";
 import NotFound from "./Pages/NotFound";
 import { LoginModal, RegisterModal } from "./Components";
-import { AdminPage } from "./Pages";
+import { AdminPage, ProductPage } from "./Pages";
 import "./App.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route path="/" element={<Home />}></Route>
-      <Route path="Products" element={<Products />}></Route>
-      <Route path="Admin" element={<AdminPage />}></Route>
-      <Route path="Login" element={<LoginModal />}></Route>
-      <Route path="Register" element={<RegisterModal />}></Route>
+      <Route path="products" element={<ProductPage />}></Route>
+      <Route path="admin" element={<AdminPage />}></Route>
+      <Route path="login" element={<LoginModal />}></Route>
+      <Route path="register" element={<RegisterModal />}></Route>
       <Route path="*" element={<NotFound />} />
     </Route>,
   ),
