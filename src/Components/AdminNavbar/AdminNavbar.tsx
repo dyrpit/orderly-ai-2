a48bad3853572e54f8e3f88f2b572b5e0ab54c7f
justynalem <102022599+justynalem@users.noreply.github.com>
2023-09-20 10:30:00 +0200

    context into one store fix (#69)

--- src/Components/AdminNavbar/AdminNavbar.tsx
+++ src/Components/AdminNavbar/AdminNavbar.tsx
@@ -2,56 +2,56 @@ import { ANButton, ANContainer, ANList } from "./AdminNavbar.styles";
 import Fade from "@mui/material/Fade";
 import { useState, useContext } from "react";
 import { AdminNavbarListCategory } from "../AdminNavbarListCategory/AdminNavbarListCategory";
-import { VariableContext } from "../../Context/variableContext/VariableContextProvider";
+import { OrderAiContext } from "../../Context/ContextProvider";
 
 export function AdminNavbar() {
- const [isListVisible, setListVisible] = useState(false);
- const [listDataCategories, setListDataCategories] = useState<string[]>([]);
+   const [isListVisible, setListVisible] = useState(false);
+   const [listDataCategories, setListDataCategories] = useState<string[]>([]);
 
- const toggleListVisibility = () => {
-  if (!isListVisible) generateRandomCategories();
-  setListVisible(!isListVisible);
- };
+   const toggleListVisibility = () => {
+      if (!isListVisible) generateRandomCategories();
+      setListVisible(!isListVisible);
+   };
 
- const generateRandomCategories = () => {
-  const items: string[] = [];
-  const randomInteger = Math.floor(Math.random() * (10 - 1) + 1);
+   const generateRandomCategories = () => {
+      const items: string[] = [];
+      const randomInteger = Math.floor(Math.random() * (10 - 1) + 1);
 
-  for (let i = 1; i <= randomInteger; i++) {
-   items.push(`Category ${i}`);
-  }
-  setListDataCategories(items);
-  console.log(items);
- };
+      for (let i = 1; i <= randomInteger; i++) {
+         items.push(`Category ${i}`);
+      }
+      setListDataCategories(items);
+      console.log(items);
+   };
 
- const generateRandomElements = () => {
-  const elements: string[] = [];
-  const randomInteger = Math.floor(Math.random() * (10 - 1) + 1);
+   const generateRandomElements = () => {
+      const elements: string[] = [];
+      const randomInteger = Math.floor(Math.random() * (10 - 1) + 1);
 
-  for (let i = 1; i <= randomInteger; i++) {
-   const category = `Category ${i}`;
-   elements.push(category);
-  }
-  return elements;
- };
+      for (let i = 1; i <= randomInteger; i++) {
+         const category = `Category ${i}`;
+         elements.push(category);
+      }
+      return elements;
+   };
 
- const { handleEditUserVisible } = useContext(VariableContext);
- const toggleeditUsersVisible = () => {
-    handleEditUserVisible();
- }
+   const { handleEditUserVisible } = useContext(OrderAiContext);
+   const toggleeditUsersVisible = () => {
+      handleEditUserVisible();
+   };
 
- return (
-  <ANContainer>
-   <ANButton>Add Category</ANButton>
-   <ANButton onClick={toggleListVisibility}>Categories</ANButton>
-   <Fade in={isListVisible} unmountOnExit>
-    <ANList>
-     {listDataCategories.map((item, index) => (
-      <AdminNavbarListCategory key={index} category={item} elements={generateRandomElements()} />
-     ))}
-    </ANList>
-   </Fade>
-   <ANButton onClick={toggleeditUsersVisible}>Edit Users</ANButton>
-  </ANContainer>
- );
+   return (
+      <ANContainer>
+         <ANButton>Add Category</ANButton>
+         <ANButton onClick={toggleListVisibility}>Categories</ANButton>
+         <Fade in={isListVisible} unmountOnExit>
+            <ANList>
+               {listDataCategories.map((item, index) => (
+                  <AdminNavbarListCategory key={index} category={item} elements={generateRandomElements()} />
+               ))}
+            </ANList>
+         </Fade>
+         <ANButton onClick={toggleeditUsersVisible}>Edit Users</ANButton>
+      </ANContainer>
+   );
 }
--- src/Context/useOrderAi.ts
+++ src/Context/useOrderAi.ts
@@ -1,46 +1,108 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { OrderAiContext } from "./ContextProvider";
+import { Category, Item, User } from "./types";
 
 export const useOrderAi = () => {
- // Login/Sign In/Admin Panel
- const [showButtons, setShowButtons] = useState("none");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showButtons, setShowButtons] = useState("none");
+  const [currentModal, setCurrentModal] = useState("none");
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [items, setItems] = useState<Item[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [editUsersVisible, setEditUserVisible] = useState(false);
 
- const showHideLoginButtons = () => {
-  if (showButtons === "none") {
-   setShowButtons("block");
-   setIsModalOpen(false);
-   changeModal("none");
-  } else {
-   setShowButtons("none");
-   setIsModalOpen(false);
-   changeModal("none");
-  }
- };
+  useEffect(() => {
+    fetch("/src/Data/categories.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data.categories);
+      })
+      .catch((error) => {
+        console.error("Error fetching JSON data:", error);
+      });
+  }, []);
 
- const [isModalOpen, setIsModalOpen] = useState(false);
- const handleSignInClick = () => {
-  setIsModalOpen(true);
-  changeModal("Sign In");
-  if (isModalOpen === true) {
-   setIsModalOpen(false);
-   changeModal("none");
-  }
- };
+  useEffect(() => {
+    fetch("/src/Data/items.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data.items);
+      })
+      .catch((error) => {
+        console.error("Error fetching JSON data:", error);
+      });
+  }, []);
 
- const [currentModal, setCurrentModal] = useState("none");
- const changeModal = (element: string) => {
-  setCurrentModal(element);
- };
- return {
-  showButtons,
-  showHideLoginButtons,
-  isModalOpen,
-  handleSignInClick,
-  currentModal,
-  changeModal,
- };
+  useEffect(() => {
+    fetch("/src/Data/users.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.users);
+      })
+      .catch((error) => {
+        console.error("Error fetching JSON data:", error);
+      });
+  }, []);
+
+  const handleUserChange = () => {
+    // Implement user data changes as needed
+  };
+
+  const handleItemChange = () => {
+    // Implement item data changes as needed
+  };
+
+  const handleCategoryChange = () => {
+    // Implement user data changes as needed
+  };
+
+  const showHideLoginButtons = () => {
+    if (showButtons === "none") {
+      setShowButtons("block");
+      setIsModalOpen(false);
+      changeModal("none");
+    } else {
+      setShowButtons("none");
+      setIsModalOpen(false);
+      changeModal("none");
+    }
+  };
+
+  const handleSignInClick = () => {
+    setIsModalOpen(true);
+    changeModal("Sign In");
+    if (isModalOpen === true) {
+      setIsModalOpen(false);
+      changeModal("none");
+    }
+  };
+
+  const changeModal = (element: string) => {
+    setCurrentModal(element);
+  };
+
+  const handleEditUserVisible = () => {
+    setEditUserVisible(!editUsersVisible);
+  };
+
+  return {
+    isModalOpen,
+    showButtons,
+    currentModal,
+    categories,
+    items,
+    users,
+    editUsersVisible,
+    showHideLoginButtons,
+    handleSignInClick,
+    changeModal,
+    handleCategoryChange,
+    handleItemChange,
+    handleUserChange,
+    handleEditUserVisible,
+  };
 };
 
 export const useOrderAiContext = () => {
- return useContext(OrderAiContext);
+  return useContext(OrderAiContext);
 };
