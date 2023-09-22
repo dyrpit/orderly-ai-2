b0c35fbb2c58fc23815305c622d182c5cd2a9a6f
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-09-22 13:23:07 +0200

    Przerzucenie buttonow z top baru do navbaru

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -7,82 +7,103 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { OrderAiContext } from "../../Context/ContextProvider";
 import "./Menu.css";
+import { SignIn } from "..";
+import { SignOut } from "../SignButtons/SignOut";
+import { AdminPanel } from "../SignButtons/AdminPanel";
 
 const imgStyle = {
-  width: "100%",
-  height: "100%",
+ width: "100%",
+ height: "100%",
 };
 
 export const Menu = () => {
-  const { showHideLoginButtons } = useContext(OrderAiContext);
-  const [open, setOpen] = useState(false);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const optionButtons = [
-    {
-      name: "Import",
-      img: "../../../src/assets/clarity_import-line.png",
-    },
-    {
-      name: "Export",
-      img: "../../../src/assets/clarity_export-line.png",
-    },
-    {
-      name: "Login",
-      img: "../../../src/assets/clarity_avatar-line.png",
-      showHideLoginButtons,
-    },
-  ];
-
-  return (
-    <Box>
-      <StyledMenu>
-        {optionButtons.map(({ img, name, showHideLoginButtons }) => (
-          <StyledIconButton onClick={showHideLoginButtons} key={name}>
-            <img src={img} />
-          </StyledIconButton>
-        ))}
-      </StyledMenu>
-      {open ? (
-        <StyledIconButtonMenu onClick={handleDrawerClose}
-          sx={{ display: "flex", justifyContent: "end" }}
-        >{<StyledCloseIcon />}</StyledIconButtonMenu>
-      ) : (
-        <StyledIconButtonMenu onClick={handleDrawerOpen}>{<StyledMenuIcon />}</StyledIconButtonMenu>
-      )}
-      <StyledDrawer variant="persistent" anchor="right" open={open}>
-        <Divider />
-        <List>
-          {optionButtons.map(({ img, name }) => (
-            <ListItem key={name}>
-              <ListItemButton>
-                <ListItemIcon>
-                  <img src={img} />
-                </ListItemIcon>
-                <ListItemText>{name}</ListItemText>
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </StyledDrawer>
-    </Box>
-  );
+ const { changeModal } = useContext(OrderAiContext);
+ const [showButtons, setShowButtons] = useState("none");
+ const [open, setOpen] = useState(false);
+ const handleDrawerOpen = () => {
+  setOpen(true);
+ };
+ const handleDrawerClose = () => {
+  setOpen(false);
+ };
+ const showHideLoginButtons = () => {
+  if (showButtons === "none") {
+   setShowButtons("block");
+   changeModal("none");
+  } else {
+   setShowButtons("none");
+   changeModal("none");
+  }
+ };
+ const optionButtons = [
+  {
+   name: "Import",
+   img: "../../../src/assets/clarity_import-line.png",
+  },
+  {
+   name: "Export",
+   img: "../../../src/assets/clarity_export-line.png",
+  },
+  {
+   name: "Login",
+   img: "../../../src/assets/clarity_avatar-line.png",
+   showHideLoginButtons,
+  },
+ ];
+ const buttonsContainerStyles = {
+  marginLeft: "auto",
+  display: showButtons,
+  position: "absolute",
+  backgroundColor: "#5C358E",
+ };
+ return (
+  <Box>
+   <StyledMenu>
+    {optionButtons.map(({ img, name, showHideLoginButtons }) => (
+     <StyledIconButton onClick={showHideLoginButtons} key={name}>
+      <img src={img} />
+     </StyledIconButton>
+    ))}
+   </StyledMenu>
+   {open ? (
+    <StyledIconButtonMenu onClick={handleDrawerClose} sx={{ display: "flex", justifyContent: "end" }}>
+     {<StyledCloseIcon />}
+    </StyledIconButtonMenu>
+   ) : (
+    <StyledIconButtonMenu onClick={handleDrawerOpen}>{<StyledMenuIcon />}</StyledIconButtonMenu>
+   )}
+   <StyledDrawer variant="persistent" anchor="right" open={open}>
+    <Divider />
+    <List>
+     {optionButtons.map(({ img, name }) => (
+      <ListItem key={name}>
+       <ListItemButton>
+        <ListItemIcon>
+         <img src={img} />
+        </ListItemIcon>
+        <ListItemText>{name}</ListItemText>
+       </ListItemButton>
+      </ListItem>
+     ))}
+    </List>
+   </StyledDrawer>
+   <Box sx={buttonsContainerStyles}>
+    <SignIn />
+    <SignOut />
+    <AdminPanel />
+   </Box>
+  </Box>
+ );
 };
 
 export const LogoContainer = () => {
-  return (
-    <Link to="/">
-      <StyledLogoContainer>
-        <div className="logo-wrapper">
-          <img style={imgStyle}
-            src="../../../src/assets/logo.png"
-          />
-        </div>
-      </StyledLogoContainer>
-    </Link>
-  );
+ return (
+  <Link to="/">
+   <StyledLogoContainer>
+    <div className="logo-wrapper">
+     <img style={imgStyle} src="../../../src/assets/logo.png" />
+    </div>
+   </StyledLogoContainer>
+  </Link>
+ );
 };
