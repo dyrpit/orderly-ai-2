63e2065b87db3f5a2dbecf5f5aa0c32b1675e0ab
Justyna <lempickajustyna@gmail.com>
2023-09-24 15:42:48 +0200

    add protected route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import RootLayout from "./Layout/RootLayout";
-import { AddCategory, EditItem, AddItem, LoginModal, RegisterModal, UsersEdit, EditCategory } from "./Components";
+import { AddCategory, EditItem, AddItem, LoginModal, RegisterModal, UsersEdit, EditCategory, ProtectedRoute } from "./Components";
 import { AdminPage, Home, ProductPage, NotFound } from "./Pages";
 import "./App.css";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />}></Route>
-      <Route path=":categoryName" element={<ProductPage />}></Route>
-      <Route path="admin" element={<AdminPage />}>
-        <Route path="addcategory" element={<AddCategory />} />
-        <Route path="editcategory/:id" element={<EditCategory />} />"
-        <Route path="edituser" element={<UsersEdit />} />
-        <Route path="additem" element={<AddItem />} />
-        <Route path="edititem/:id" element={<EditItem />} />"
-      </Route>
-      <Route path="login" element={<LoginModal />}></Route>
-      <Route path="register" element={<RegisterModal />}></Route>
-      <Route path="*" element={<NotFound />} />
-    </Route>,
-  ),
+ createRoutesFromElements(
+  <Route path="/" element={<RootLayout />}>
+   <Route index element={<Home />}></Route>
+   <Route path=":categoryName" element={<ProductPage />}></Route>
+   <Route
+    path="admin"
+    element={
+     <ProtectedRoute>
+      <AdminPage />
+     </ProtectedRoute>
+    }>
+    <Route path="addcategory" element={<AddCategory />} />
+    <Route path="editcategory/:id" element={<EditCategory />} />
+    <Route path="edituser" element={<UsersEdit />} />
+    <Route path="additem" element={<AddItem />} />
+    <Route path="edititem/:id" element={<EditItem />} />
+   </Route>
+   <Route path="login" element={<LoginModal />}></Route>
+   <Route path="register" element={<RegisterModal />}></Route>
+   <Route path="*" element={<NotFound />} />
+  </Route>,
+ ),
 );
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+ return (
+  <>
+   <RouterProvider router={router} />
+  </>
+ );
 }
 export default App;
