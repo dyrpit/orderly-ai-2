0a53c2759af9fd06b251d73d2897f5d55676695a
justynalem <102022599+justynalem@users.noreply.github.com>
2023-09-25 13:16:23 +0200

    protected routes fixes, changing role fixes (#113)

--- src/Components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,13 @@
 import { Navigate } from "react-router-dom";
 import useDecrypt from "../../Hooks/useDecrypt";
-import { UserRole } from "../../Context/types";
+import { User } from "../../Context/types";
 import { ReactNode } from "react";
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
  const { parseJwtToken } = useDecrypt();
- const user: { role: UserRole } | undefined = parseJwtToken();
+ const user: User | undefined = parseJwtToken();
 
- if (!user || user.role !== UserRole.admin) {
+ if (!user) {
   return <Navigate to="/" replace />;
  }
 
