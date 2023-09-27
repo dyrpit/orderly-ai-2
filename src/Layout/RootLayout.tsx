cb20898da574f8e7d2b7e6f60a89e20ffbeec33b
Azzirr <114509095+Azzirr@users.noreply.github.com>
2023-09-27 14:42:38 +0200

    User session expire (#161)

--- src/Layout/RootLayout.tsx
+++ src/Layout/RootLayout.tsx
@@ -2,17 +2,19 @@ import { Outlet } from "react-router-dom";
 import { Footer, MainContainer, ModalsWrapper } from "../Components";
 import { NavBar } from "../Components/NavBar/NavBar";
 import { TopBar } from "../Components/index";
+import { useUserSession } from "../Hooks/useUserSession";
 
 export default function RootLayout() {
-  return (
-    <div className="RootLayout">
-      <NavBar />
-      <MainContainer>
-        <TopBar></TopBar>
-        <ModalsWrapper></ModalsWrapper>
-        <Outlet />
-      </MainContainer>
-      <Footer></Footer>
-    </div>
-  );
+ useUserSession();
+ return (
+  <div className="RootLayout">
+   <NavBar />
+   <MainContainer>
+    <TopBar></TopBar>
+    <ModalsWrapper></ModalsWrapper>
+    <Outlet />
+   </MainContainer>
+   <Footer></Footer>
+  </div>
+ );
 }
