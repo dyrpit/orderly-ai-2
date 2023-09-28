718132c89ff0af633193e2be4f99d650522f60ca
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-09-28 14:05:49 +0200

    Ready for deploy

--- src/Context/types.ts
+++ src/Context/types.ts
@@ -1,33 +1,33 @@
 export enum UserRole {
-  admin = "admin",
-  user = "user",
-  regular = "regular"
+ admin = "admin",
+ user = "user",
+ regular = "regular",
 }
 export interface Category {
-  name: string;
-  description: string;
-  colour: string;
+ name: string;
+ description: string;
+ colour: string;
 }
 
 export interface ProductData {
-  id: number;
-  name: string;
-  website: string;
-  license: string;
-  youtubeUrl: string;
-  description: string;
+ id: number;
+ name: string;
+ website: string;
+ license: string;
+ youtubeUrl: string;
+ description: string;
 }
 
 export interface CategoryData {
-  id: number;
-  name: string;
-  color: string;
-  imageUrl: string;
-  products: ProductData[];
+ id: number;
+ name: string;
+ color: string;
+ imageUrl: string;
+ products: ProductData[];
 }
 export interface User {
-  id: number;
-  email: string;
-  password: string;
-  role: UserRole;
+ id: number;
+ email: string;
+ password: string;
+ role: UserRole;
 }
